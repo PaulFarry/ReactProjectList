4ccd16284f56a1f73821b6fa163b7d24829a55c0
paulfarry <paulfarry@gmail.com>
2023-06-14 10:17:24 +1000

    Lab 04 Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import '../node_modules/mini.css/dist/mini-default.min.css';
-
+import ProjectsPage from './projects/ProjectsPage';
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
+    <div className='container'>
+      <ProjectsPage />
+    </div>
   );
 }
 
